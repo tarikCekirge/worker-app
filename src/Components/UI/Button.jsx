99a0cf479efb14b57bab4c575c960906765af856
Tarik Cekirge <tcekirge@gmail.com>
2024-03-10 20:47:55 +0300

    Add error handling and validation in AddWorker component

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -5,7 +5,7 @@ const Button = (props) => {
     text-center text-white py-2 px-4 bg-slate-500 w-full rounded-md mt-4 font-bold
     `;
   return (
-    <Button type={props.type} className={props.className}>
+    <Button onClick={props.onClick} type={props.type} className={props.className}>
       {props.children}
     </Button>
   );
--- src/Components/Workers/AddWorker.jsx
+++ src/Components/Workers/AddWorker.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddWorker = (props) => {
   const [workerData, setWorkerData] = useState({
@@ -8,6 +9,7 @@ const AddWorker = (props) => {
     salary: Number(""),
     id: null,
   });
+  const [error, SetError] = useState();
 
   const minSalary = 10000;
 
@@ -19,13 +21,27 @@ const AddWorker = (props) => {
   const addWorkerHandler = (e) => {
     e.preventDefault();
     const { name, salary } = workerData;
-    if (name.trim().length === 0 || salary.trim().length === 0) {
-      alert("Tüm alanları doldurunuz.");
+    if (name.trim().length === 0) {
+      SetError({
+        title: "İsim Alanı Zorunludur",
+        message: "Lütfen isim giriniz",
+      });
+      return;
+    }
+    const salaryString = salary.toString();
+    if (salaryString.trim().length === 0) {
+      SetError({
+        title: "Maaş Alanı Zorunludur",
+        message: `Lütfen ${minSalary}'den büyük bir maaş değeri giriniz`,
+      });
       return;
     }
 
     if (+salary < minSalary) {
-      alert(`${minSalary}'den küçük maaş girilemez`);
+      SetError({
+        title: "Maaş Alanı Zorunludur",
+        message: `Lütfen ${minSalary}'den büyük bir maaş değeri giriniz`,
+      });
       return;
     }
 
@@ -40,25 +56,32 @@ const AddWorker = (props) => {
     });
   };
 
+  const errorHandler = () => {
+    SetError(null);
+  };
+
   return (
-    <Card className="mt-10">
-      <h1 className="text-center text-2xl font-bold text-white">Maaş Otomasyonu</h1>
-      <form onSubmit={addWorkerHandler}>
-        <div className="grid gap-1">
-          <label className="text-white font-semibold" htmlFor="name">
-            Çalışan İsmi
-          </label>
-          <input className=" p-2 rounded-md" type="text" id="name" placeholder="Çalışan ismi girin" value={workerData.name} onChange={onChangeWorkerHandler} />
-        </div>
-        <div className="grid gap-1">
-          <label className="text-white font-semibold" htmlFor="salary">
-            Maaş
-          </label>
-          <input className=" p-2 rounded-md" type="number" id="salary" min={minSalary} placeholder="Maaş girin" value={workerData.salary} onChange={onChangeWorkerHandler} />
-        </div>
-        <Button type="submit">Ekle</Button>
-      </form>
-    </Card>
+    <>
+      {error && <ErrorModal onConfirm={errorHandler} error={error} />}
+      <Card className="mt-10">
+        <h1 className="text-center text-2xl font-bold text-white">Maaş Otomasyonu</h1>
+        <form onSubmit={addWorkerHandler}>
+          <div className="grid gap-1">
+            <label className="text-white font-semibold" htmlFor="name">
+              Çalışan İsmi
+            </label>
+            <input className=" p-2 rounded-md" type="text" id="name" placeholder="Çalışan ismi girin" value={workerData.name} onChange={onChangeWorkerHandler} />
+          </div>
+          <div className="grid gap-1">
+            <label className="text-white font-semibold" htmlFor="salary">
+              Maaş
+            </label>
+            <input className=" p-2 rounded-md" type="number" id="salary" placeholder="Maaş girin" value={workerData.salary} onChange={onChangeWorkerHandler} />
+          </div>
+          <Button type="submit">Ekle</Button>
+        </form>
+      </Card>
+    </>
   );
 };
 
